5e9655fbd438acc91a88fa91f33a2a1ac070df43
Justin <justinrvaldez@gmail.com>
2026-07-07 17:54:44 -0600

    Error handling added to extractor function

--- weather_etl/extractor.py
+++ weather_etl/extractor.py
@@ -1,19 +1,24 @@
 import requests
-import pprint
+import logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+def extractor():
 
-url = "https://api.open-meteo.com/v1/forecast"
+    url = "https://api.open-meteo.com/v1/forecast"
 
-params = {
-	"latitude": 35.924086,
-	"longitude": -106.791974,
-	"hourly": ["temperature_2m", "precipitation_probability"],
-	"temperature_unit": "fahrenheit",
-}
+    params = {
+        "latitude": 35.924086,
+        "longitude": -106.791974,
+        "hourly": ["temperature_2m", "precipitation_probability"],
+        "temperature_unit": "fahrenheit",
+    }
 
-response = requests.get(url, params = params)
-
-data = response.json()
-
-print(data)
-print("Time:", data['hourly']['time'][0] + " " + data['hourly_units']['time'])
-print(str(data['hourly']['temperature_2m'][0]) + data['hourly_units']['temperature_2m'])
+    try:
+        response = requests.get(url, params = params, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Extraction failed: {e}")
+        raise 
+    
+print(extractor())
