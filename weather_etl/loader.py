e7d8a8d8dc637016c785eb3c6bb0eda75c4c9be0
Justin <justinrvaldez@gmail.com>
2026-07-09 12:37:54 -0600

    postgresql connection extablished through python

--- weather_etl/loader.py
+++ weather_etl/loader.py
@@ -1,11 +1,27 @@
+import psycopg
+import datetime
+import os
+
 from transformer import transformer
 from extractor import extractor
-import datetime
+from dotenv import load_dotenv
 
-data = extractor()
+load_dotenv()
 
-issue_date = datetime.datetime.now(datetime.timezone.utc)
-schema_location, readings = transformer(data, issue_date)
+# issue_date = datetime.datetime.now(datetime.timezone.utc)
+# schema_location, readings = transformer(extractor(), issue_date)
 
-print(schema_location)
-print(readings)
+connection = psycopg.connect(
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
+)
+
+cursor = connection.cursor()
+cursor.execute("SELECT version();")
+print(cursor.fetchone())
+
+cursor.close()
+connection.close()
